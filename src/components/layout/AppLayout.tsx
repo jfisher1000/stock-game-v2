26bbaa6cf0c8baa6ed962d503fbb56f846b441cd
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-02 15:55:31 -0700

    Update AppLayout.tsx

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,29 +1,17 @@
-import { Outlet, useNavigate } from 'react-router-dom';
-import Navbar from './Navbar';
-import { useIdleTimer } from '@/hooks/useIdleTimer';
-import { useAuthStore } from '@/store/auth';
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import { Toaster } from "sonner"; // Import the Toaster component
 
 const AppLayout = () => {
-  const { logOut } = useAuthStore();
-  const navigate = useNavigate();
-
-  // This function will be called when the user becomes idle
-  const handleIdle = () => {
-    logOut();
-    // Redirect to the login page with a message
-    navigate('/login?sessionExpired=true');
-  };
-
-  // Set the timeout to 30 minutes (30 * 60 * 1000 = 1800000 milliseconds)
-  useIdleTimer(handleIdle, 1800000);
-
   return (
-    <div className="min-h-screen bg-background text-foreground">
+    <>
       <Navbar />
-      <main className="container mx-auto p-4 mt-8">
+      <main className="p-4 md:p-8">
         <Outlet />
       </main>
-    </div>
+      {/* Add the Toaster component here to render notifications */}
+      <Toaster richColors position="top-right" />
+    </>
   );
 };
 
