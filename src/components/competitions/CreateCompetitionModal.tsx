8b340d95c6ee2a73eb1d3b9d49a2e54b2fa7f624
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-02 14:00:05 -0700

    Update CreateCompetitionModal.tsx

--- src/components/competitions/CreateCompetitionModal.tsx
+++ src/components/competitions/CreateCompetitionModal.tsx
@@ -37,7 +37,7 @@ const CreateCompetitionModal = ({
                 type="text"
                 id="competitionName"
                 name="competitionName"
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-gray-900"
                 required
                 value={competitionName}
                 onChange={(e) => setCompetitionName(e.target.value)}
