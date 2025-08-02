e724ffde3c2c25dab1eeb0e3fd973860a2e7e109
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-02 12:13:22 -0700

    Create DashboardPage.tsx

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+const DashboardPage = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-white">
+        Welcome to VStock Trader!
+      </h1>
+      <p className="text-gray-400 mt-4">Your journey to becoming a stock market guru starts here.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
