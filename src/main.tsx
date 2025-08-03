9d7dcd87b219dfcc232b4b2f84bda8f2a0f73cf7
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-02 18:05:34 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
+// The <BrowserRouter> has been removed from this file.
+// The App component will now manage the router internally.
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
-);
+)
