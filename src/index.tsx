a956bead41945e41cf835896d82e00b24434b9f1
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-03 18:26:01 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import * as serviceWorker from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore } from "./store/index";
+
+// Correctly importing the 'store' object that is exported from './store/index'
+import { store } from "./store/index";
 
 const getId : any = document.getElementById('root');
 const root = ReactDOM.createRoot(getId);
+
 root.render(
-  <Provider store={configureStore({})}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+    // The store is passed to the Redux Provider
+    <Provider store={store}>
+      <React.Fragment>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.Fragment>
+    </Provider>
 );
 
+serviceWorker.unregister();
