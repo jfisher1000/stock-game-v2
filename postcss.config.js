ac6fb26a417af59981a5d0cc7fcfda8fd9137fb4
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-03 17:33:12 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
-// postcss.config.js
-module.exports = {
-    plugins: {
-        // 'postcss-import': {},
-        tailwindcss: {},
-        autoprefixer: {},
-    }
-}
+// This configuration is updated to use ES Module syntax (export default)
+// to resolve the "module is not defined" error in an ES module project.
+
+export default {
+  plugins: {
+    // Standard PostCSS plugin for handling Tailwind CSS directives
+    tailwindcss: {},
+    // Standard PostCSS plugin for vendor prefixing CSS rules
+    autoprefixer: {},
+  },
+};
