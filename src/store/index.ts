1d7adb94f2ae446b58a98de78468efd29da27065
jfisher1000 <jonathan.fisher1000@gmail.com>
2025-08-03 18:25:37 -0700

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,24 +1,22 @@
-// src/store/index.ts
-
 import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
+
 import rootReducer from "./reducers";
 import rootSaga from "./sagas";
 
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
-// This line checks if the Redux DevTools Extension is installed.
-// If it is, it uses the extension's compose function.
-// If not, it falls back to the default `compose` from redux.
+// This now correctly falls back to the standard compose function
+// when the Redux DevTools extension is not available.
 const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
-  // This now safely applies the middleware with or without the devtools.
   composeEnhancers(applyMiddleware(...middlewares))
 );
 
 sagaMiddleware.run(rootSaga);
 
+// We are exporting the created 'store' instance.
 export { store };
